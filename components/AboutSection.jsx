d8ca96ac6d1c58e6a948bc0af6097cb38a222768
chandra shekar <kusumachandu43@gmail.com>
2023-10-13 15:54:08 +0530

    ✨initial commit

---
+++ components/AboutSection.jsx
@@ -0,0 +1,145 @@
+"use client";
+
+import React, { useState, useTransition } from "react";
+import Image from "next/image";
+
+import TabButton from "./TabButton";
+
+const TAB_DATA =  [
+  {
+    title:"Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>JavaScript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>Next</li>
+        <li>Express</li>
+        <li>MongoDb</li>
+        <li>Node</li>
+        <li>Git</li>
+        <li>PostgreSQL</li>
+        <li>Postman</li>
+      </ul>
+    )
+  },
+  {
+    title:"Education",
+    id: "education",
+    content: (
+      <div className="flex flex-col gap-4">
+        <ul>
+          <li className="font-semibold">Malla Reddy Engineering College  (BTech)</li>
+          <li>Jawaharlal Nehru Technological University, Hyderabad</li>
+          <li className="font-extralight"><span className="font-semibold">CGPA: 7.2</span> (2018 - 2021)</li>
+        </ul>
+        <ul>
+          <li className="font-semibold">Anurag Engineering College (Diploma)</li>
+          <li>Jawaharlal Nehru Technological University, Hyderabad</li>
+          <li className="font-extralight"><span className="font-semibold">CGPA: 8.1</span> (2015 - 2018)</li>
+        </ul>
+
+      </div>
+    )
+  },
+  {
+    title:"Certifications",
+    id: "certifications",
+    content: (
+      <ul>
+        <li className="font-semibold">Full Stack Mern Developer</li>
+        <li>Masai School <span className="font-extralight">(Apr 2021 - Apr 2022 )</span></li>
+      </ul>
+    )
+  },
+  {
+    title:"Experiences",
+    id: "experiences",
+    content: (
+      <div className="flex flex-col gap-4">
+        <ul>
+          <li className="font-semibold">React Developer</li>
+          <li>Metafic <span className="font-extralight">(May 2022 - Oct 2022 )</span></li>
+        </ul>
+        <ul>
+          <li className="font-semibold">Full Stack Web Developer</li>
+          <li>Zvoid <span className="font-extralight">(Nov 2022 - Jul 2023 )</span></li>
+        </ul>
+        <ul>
+          <li className="font-semibold">Frontend Developer</li>
+          <li>Freelance <span className="font-extralight">(Jul 2023 - Present )</span></li>
+        </ul>
+      </div>
+    )
+  },
+]
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about-image.png"
+          width={500}
+          height={500}
+          alt="about-image"
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a full stack web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working woth JavaScript, React, Redux, Node js, Express, PostgreSQL,
+            HTML, CSS and Git. I am a quick learner and I am always looking ot
+            expand my knowledge and skill set. I am a team player and I am
+            excited to work with oithers to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              active={tab === "skills"}
+              selectTab={() => handleTabChange("skills")}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              active={tab === "education"}
+              selectTab={() => handleTabChange("education")}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              active={tab === "certifications"}
+              selectTab={() => handleTabChange("certifications")}
+            >
+              Certifications
+            </TabButton>
+            <TabButton
+              active={tab === "experiences"}
+              selectTab={() => handleTabChange("experiences")}
+            >
+              Experiences
+            </TabButton>
+          </div>
+          <div className="mt-4">
+            {
+              TAB_DATA.find((t) => t.id === tab).content
+            }
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ components/ProjectSection.jsx
@@ -0,0 +1,100 @@
+"use client"
+
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "OLNX",
+    description: "AN AI powered web apllication",
+    image: "/images/projects/OLNX.png",
+    tag: ["All", "Applications"],
+    gitUrl: '/',
+    previewUrl: '/'
+  },
+  {
+    id: 2,
+    title: "Anyo",
+    description: "A website for an organisation",
+    image: "/images/projects/ANYO.png",
+    tag: ["All", "Websites"],
+    gitUrl: '/',
+    previewUrl: '/'
+  },
+  {
+    id: 3,
+    title: "Zvoid",
+    description: "Website for the Zvoid Organisation",
+    image: "/images/projects/ZVOID.png",
+    tag: ["All", "Websites"],
+    gitUrl: '/',
+    previewUrl: '/'
+  },
+  {
+    id: 4,
+    title: "Body-Shop",
+    description: "An E-Commerce application",
+    image: "/images/projects/bodyshop.png",
+    tag: ["All", "Applications"],
+    gitUrl: '/',
+    previewUrl: '/'
+  },
+  {
+    id: 5,
+    title: "Practo",
+    description: "An Health-Care web-application",
+    image: "/images/projects/Practo.png",
+    tag: ["All", "Applications"],
+    gitUrl: '/',
+    previewUrl: 'https://clone-practo.vercel.app/'
+  },
+  {
+    id: 6,
+    title: "Diagnostic",
+    description: "An Health-Care website",
+    image: "/images/projects/Diagnostic.png",
+    tag: ["All", "Websites"],
+    gitUrl: '/',
+    previewUrl: '/'
+  },
+];
+
+const ProjectSection = () => {
+
+  const [tag, setTag] = useState('All');
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  }
+
+  const filteredProjects = projectsData.filter((project) => {
+    return (project.tag.includes(tag));
+  })
+
+  return (
+    <div>
+      <h2 className="text-center font-bold text-4xl">My Projects</h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === 'All'}  /> 
+        <ProjectTag onClick={handleTagChange} name="Websites" isSelected={tag === 'Websites'}  /> 
+        <ProjectTag onClick={handleTagChange} name="Applications" isSelected={tag === 'Applications'}  /> 
+      </div>
+      <div className="mt-8 grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            imgUrl={project.image}
+            description={project.description}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSection;
